1e20457d7abd32de6b8cd2e9b9d7274e2076ba7f
piatoz <58035202+piatoz@users.noreply.github.com>
2026-09-09 14:13:59 +0200

    Age Validator

---
+++ Age Validator/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Programming_1_Assignments;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Start();
+    }
+
+    void Start ()
+    {
+        Console.WriteLine("Hi! Please enter your age");
+        string input = Console.ReadLine();
+        int age = int.Parse(input);
+    
+    // read user's age
+
+    if (age >= 18)
+    {
+        Console.Write($"You are {age} years old. Access Granted");
+    }
+    else
+    {
+        Console.Write($"You need to be at least 18. Denied");
+    }
+}
+}
